05310100dc18adb9277d0ac4730f5d8e359c16f1
Dan Savage <djm87@wildcats.unh.edu>
2022-08-05 11:50:39 -0600

    Update __init__.py

--- MILK/__init__.py
+++ MILK/__init__.py
@@ -6,20 +6,21 @@ Created on Wed Jan 20 13:43:23 2021
 @author: danielsavage
 """
 
-from MILK.data import (generateGroup,prepareData,examples,lcls)
-from MILK.interface import (parameterEditor,addPhase)
-from MILK.interface.model import (texture,sizeStrain)
+from MILK.data import (generateGroup, prepareData, examples, lcls)
+from MILK.interface import (parameterEditor, addPhase)
+from MILK.interface.model import (texture, sizeStrain)
 from MILK.MAUDText import maud
 from MILK.MAUDText.callMaudText import resource_file_path
-from MILK.environment.initilize import loadini
-import os 
+from MILK.environment.initilize import load_json
+import os
 import sys
 
 
-#Add resources to the search path 
-curpath=os.path.dirname(os.path.realpath(__file__))
-resourceList=['MILK/interface/model/resources','MILK/MAUDText/resources','resources','MILK/environment/resources']
+# Add resources to the search path
+curpath = os.path.dirname(os.path.realpath(__file__))
+resourceList = ['MILK/interface/model/resources',
+                'MILK/MAUDText/resources', 'resources', 'MILK/environment/resources']
 for resource in resourceList:
-    sys.path.append(os.path.join(curpath[0:-5],resource))
+    sys.path.append(os.path.join(curpath[0:-5], resource))
 
-myconst=1
+myconst = 1
