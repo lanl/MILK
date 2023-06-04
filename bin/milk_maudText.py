9f8f3751e7d562674bb57f950171e228cbfe1c93
Daniel Savage <7084261+djm87@users.noreply.github.com>
2023-06-03 21:27:36 -0600

    feature and bug fixes

--- bin/milk_maudText.py
+++ bin/milk_maudText.py
@@ -6,9 +6,10 @@ Created on Fri Aug  5 14:46:59 2022
 @author: danielsavage
 """
 
-import MILK
-import os
+from MILK.MAUDText import callMaudText
+from os import getenv
 import argparse
+from pathlib import Path
 
 
 def main():
@@ -17,22 +18,22 @@ def main():
         description="Run MAUD ins file.")
     parser.add_argument("-f", "--file", type=str, required=True,
                         help="Ins filename and relative path.")
-    parser.add_argument("-p", "--maud_path", type=str, default=None,
+    parser.add_argument("-p", "--maud_path", type=str, default=getenv('MAUD_PATH').strip("'"),
                         help="Path to MAUD installation if different than MAUD_PATH in environement.")
     parser.add_argument("-j", "--java_opt", type=str, default="mx8G",
                         help="Java commandline options.")
+    parser.add_argument("-s", "--simple_call", action='store_true',
+                        help="If specified no log files are output.")
+    parser.add_argument("-t", "--timeout", type=float, default=None,
+                        help="Sets maximum time in seconds for MAUD to run.")
     args = parser.parse_args()
 
-    if args.maud_path is None:
-        args.maud_path = os.getenv('MAUD_PATH')
-
-    MILK.MAUDText.callMaudText.run_MAUD(
+    callMaudText.run_MAUD(
         args.maud_path,
         args.java_opt,
-        'True',
-        None,
-        args.file
-    )
+        str(args.simple_call),
+        args.timeout,
+        args.file)
 
 
 if __name__ == "__main__":
