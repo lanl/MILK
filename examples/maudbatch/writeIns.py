4e0d69e9941e720fb17fb31480d55b997b6c370f
Daniel Savage <zf1005@wildcats.unh.edu>
2023-06-19 09:53:48 -0600

    Update writeIns.py

--- examples/maudbatch/writeIns.py
+++ examples/maudbatch/writeIns.py
@@ -1,19 +1,25 @@
 import os
+import sys
+from pathlib import Path
 
-fname = os.path.join('fecu.ins')
-fID = open(fname, "w")
+FNAME = 'fecu.ins'
+with open(FNAME, "w") as fID:
+    fID.write('_maud_working_directory\n')
 
-fID.write('_maud_working_directory\n')
-fullpath = r'\'' + os.getcwd() + '/\'\n'
-fID.write(fullpath)
-fID.write('\n')
-fID.write('loop_\n')
-fID.write('_riet_analysis_file\n')
-fID.write('_riet_analysis_iteration_number\n')
-fID.write('_riet_analysis_wizard_index\n')
-fID.write('_riet_analysis_fileToSave\n')
-fID.write('_riet_meas_datafile_name\n')
-fID.write('_riet_append_simple_result_to\n')
-fID.write('\'FeCustart.par\' 7 13 \'FECU1010.par\' \'FECU1010.UDF\' \'FECUresults.txt\'\n')
+    fullpath = f"{Path().cwd()}{os.sep}"
+    if "win" in sys.platform:
+        fullpath = fullpath.replace('\\','\\\\')
 
-fID.close() 
+    fID.write(fullpath+'\n')
+    fID.write('\n')
+    fID.write('\n')
+    fID.write('loop_\n')
+    fID.write('_riet_analysis_file\n')
+    fID.write('_riet_analysis_iteration_number\n')
+    fID.write('_riet_analysis_wizard_index\n')
+    fID.write('_riet_analysis_fileToSave\n')
+    fID.write('_riet_meas_datafile_name\n')
+    fID.write('_riet_append_simple_result_to\n')
+    fID.write('\'FeCustart.par\' 7 13 \'FECU1010.par\' \'FECU1010.UDF\' \'FECUresults.txt\'\n')
+
+ 
