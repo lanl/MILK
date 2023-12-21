f149001630447b5c638f182838a6a455efd8301a
Daniel Savage <7084261+djm87@users.noreply.github.com>
2023-12-21 11:58:06 -0700

    Update MAUD parallel

---
+++ MILK/parallel.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 10:41:10 2022.
+
+@author: danielsavage
+"""
+# Setup integration
+from pathos.pools import ProcessPool as pPool
+from functools import partial
+from tqdm import tqdm
+
+def serial_map(func, poolsize: int, disable_tqdm: bool, loop_arg:list, *args):
+    """Do parallel computing with function and tqdm progress bar."""
+    # map_size = len(loop_arg)
+    # if map_size==1:
+    #     func2 = partial(func,*args)
+    #     return list(func2(loop_arg[0]))
+    # else:
+    mapper = map
+    return list(tqdm(mapper(partial(func,*args),
+                            loop_arg), total=len(loop_arg),disable=disable_tqdm))
+
+
+def parallel_map(func, poolsize: int, disable_tqdm: bool, loop_arg:list, *args):
+    """Do parallel computing with function and tqdm progress bar."""
+    if poolsize == 1:
+        return serial_map(func, poolsize, disable_tqdm, loop_arg, *args)
+
+    with pPool(poolsize) as pool:
+        mapper = pool.imap
+        return list(tqdm(mapper(partial(func,*args),
+                                loop_arg), total=len(loop_arg),disable=disable_tqdm))
