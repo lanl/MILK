1e41c467ccc5904fe3eff343b5255989af762cc0
Daniel Savage <7084261+djm87@users.noreply.github.com>
2024-04-25 11:39:02 -0600

    Support for zip added

--- MILK/parallel.py
+++ MILK/parallel.py
@@ -7,6 +7,7 @@ Created on Tue Feb 15 10:41:10 2022.
 """
 # Setup integration
 from pathos.pools import ProcessPool as pPool
+from pathos.helpers import cpu_count
 from functools import partial
 from tqdm import tqdm
 
@@ -18,6 +19,7 @@ def serial_map(func, poolsize: int, disable_tqdm: bool, loop_arg:list, *args):
     #     return list(func2(loop_arg[0]))
     # else:
     mapper = map
+    loop_arg = list(loop_arg)
     return list(tqdm(mapper(partial(func,*args),
                             loop_arg), total=len(loop_arg),disable=disable_tqdm))
 
@@ -29,5 +31,9 @@ def parallel_map(func, poolsize: int, disable_tqdm: bool, loop_arg:list, *args):
 
     with pPool(poolsize) as pool:
         mapper = pool.imap
+        loop_arg = list(loop_arg)
         return list(tqdm(mapper(partial(func,*args),
                                 loop_arg), total=len(loop_arg),disable=disable_tqdm))
+
+def get_ncpus():
+    return cpu_count()
